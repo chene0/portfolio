73d3d3809091a87fef849b0d431c09227f93ee32
Ethan Chen <ethansunchen@gmail.com>
2024-08-23 19:33:18 -0600

    Add footer with contacts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-neutral min-w-full">
+    <html lang="en" className="bg-neutral min-w-full max-w-full">
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { motion, useAnimate, useScroll, useTransform } from "framer-motion";
 import { useInView } from "framer-motion";
 import { Link } from 'react-scroll';
 import Image from 'next/image';
+import { MdEmail } from "react-icons/md";
 
 export default function Home() {
   const container = {
@@ -67,26 +68,28 @@ export default function Home() {
   const scrollToProjects = useRef();
   const { scrollYProgress } = useScroll()
   const scale = useTransform(scrollYProgress, [0, 1], [1, 1.1]);
+  const y = useTransform(scrollYProgress, [0, 1], [0, 1000]);
 
   return (
-    <div className='bg-neutral relative'>
+    <div id='top' className='bg-neutral relative'>
       <div className="sticky top-0 z-50 navbar bg-base-100">
         <div className="navbar-center mx-auto hidden lg:flex">
           <ul className="menu menu-horizontal mx-auto px-1">
-            <li><Link to='introduction' smooth>Welcome</Link></li>
+            <li><Link to='top' smooth>Welcome</Link></li>
             <li><Link to='projects' smooth>Projects</Link></li>
             <li><Link to='awards' smooth>Awards</Link></li>
+            <li><Link to='footer' smooth>Contact</Link></li>
           </ul>
         </div>
       </div>
       <div className="prose lg:prose-xl divide-y divide-solid bg-neutral min-w-full">
         {/* INTRODUCTION */}
-        <div className="min-h-screen mx-auto relative z-10">
+        <div className="min-h-screen mx-auto relative z-10" style={{ overflow: 'hidden' }}>
           {/* CLOUD BACKGROUND */}
           <motion.div
             id="introduction"
             className='absolute -top-32 z-0'
-            style={{ scale: scale }}
+            style={{ y: y, scale: scale }}
           >
             <motion.svg className='z-0' width="100vw" height="100vh" viewBox="0 0 1440 863" fill="none" xmlns="http://www.w3.org/2000/svg" initial="hidden" animate="visible">
               <motion.path strokeWidth={2}
@@ -126,7 +129,7 @@ export default function Home() {
               variants={item}
             >
               <div>
-                <h4 className="text-center">An aspiring full-stack developer</h4>
+                <h4 className="text-center">An aspiring full-stack developer studying<br />Computer Engineering at the University of Waterloo</h4>
               </div>
             </motion.li>
             <motion.li
@@ -272,7 +275,7 @@ export default function Home() {
                 </figure>
                 <div className="card-body">
                   <h2 className="card-title">Untitled First Person Shooter</h2>
-                  <p>Rely on your audio and attentiveness to thrive in this eerie shooter</p>
+                  <p>Rely on your audio and attentiveness to thrive in this eerie shooter.</p>
                   <div className="card-actions justify-end">
                     <a
                       target="_blank"
@@ -402,6 +405,40 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        {/* CONTACT */}
+        <footer id='footer' className="footer bg-neutral text-neutral-content items-center px-40 py-4">
+          <aside className="grid-flow-col items-center">
+            <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+          </aside>
+          <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <a href="https://www.linkedin.com/in/ethan-chen-9b214329a/" target="_blank" rel="noopener noreferrer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current">
+                <path
+                  d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"></path>
+              </svg>
+            </a>
+            <a href="https://github.com/chene0" target="_blank" rel="noopener noreferrer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 98 96"
+                className="fill-current">
+                <path
+                  d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"></path>
+              </svg>
+            </a>
+            <a href='mailto:ethansunchen@gmail.com'>
+              <MdEmail size={'1.5rem'} />
+            </a>
+          </nav>
+        </footer>
       </div>
     </div>
   )
